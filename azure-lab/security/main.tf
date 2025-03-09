9ad689ed0d2dfec8cd6f17c006b9b12521264fc7
Jon Hill <jonathan.hill@lpnt.net>
2025-03-09 17:44:40 -0400

    Removed old vault.

--- azure-lab/security/main.tf
+++ azure-lab/security/main.tf
@@ -149,24 +149,6 @@ resource "azurerm_storage_container" "tfstate" {
 # --------------------------------------------------
 # Secure Vault
 # --------------------------------------------------
-module "vault" {
-  source                     = "../../modules/azurerm/security/vault"
-  key_vault_name             = var.vault_name
-  resource_group_name        = azurerm_resource_group.security.name
-  location                   = "eastus"
-  sku_name                   = "standard"
-  purge_protection           = false
-  soft_delete_retention_days = 90
-
-  tenant_id = var.tenant_id
-
-  providers = {
-    azurerm = azurerm.lab
-  }
-
-  depends_on = [azurerm_resource_group.security]
-}
-
 module "security_vault" {
   source                     = "../../modules/azurerm/security/vault"
   key_vault_name             = var.security_vault_name
@@ -206,27 +188,6 @@ module "networking_vault" {
 # --------------------------------------------------
 # Secure Vault Access (Azure Admin Account)
 # --------------------------------------------------
-module "vault_access" {
-  source       = "../../modules/azurerm/security/vault-access"
-  key_vault_id = module.vault.key_vault_id
-
-  access_policies = [
-    {
-      tenant_id               = var.tenant_id
-      object_id               = var.admin_object_id
-      key_permissions         = ["Get", "List"]
-      secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
-      certificate_permissions = ["Get", "List"]
-    }
-  ]
-
-  providers = {
-    azurerm = azurerm.lab
-  }
-
-  depends_on = [module.vault]
-}
-
 module "security_vault_access" {
   source       = "../../modules/azurerm/security/vault-access"
   key_vault_id = module.security_vault.key_vault_id
@@ -273,27 +234,6 @@ module "networking_vault_access" {
 # --------------------------------------------------
 # Secure Vault Access (Service Principal)
 # --------------------------------------------------
-module "sp_vault_access" {
-  source       = "../../modules/azurerm/security/vault-access"
-  key_vault_id = module.vault.key_vault_id
-
-  access_policies = [
-    {
-      tenant_id               = var.tenant_id
-      object_id               = var.sp_object_id
-      key_permissions         = ["Get", "List"]
-      secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
-      certificate_permissions = ["Get", "List"]
-    }
-  ]
-
-  providers = {
-    azurerm = azurerm.lab
-  }
-
-  depends_on = [module.vault]
-}
-
 module "security_sp_vault_access" {
   source       = "../../modules/azurerm/security/vault-access"
   key_vault_id = module.security_vault.key_vault_id
@@ -359,7 +299,6 @@ module "security_secrets" {
     "securityspobjectid"       = ""
     "storageaccount"           = ""
     "tenantid"                 = ""
-    "vaultname"                = ""
     "securityvaultname"        = ""
     "githubtoken"              = ""
   }
@@ -377,7 +316,6 @@ module "networking_secrets" {
   secrets = {
     "devopspat"                = ""
     "devopsorgname"            = ""
-    "networkingvaultname"      = ""
     "adminobjectid"            = ""
     "backendContainer"         = ""
     "backendResourceGroup"     = ""
@@ -397,7 +335,7 @@ module "networking_secrets" {
     azurerm = azurerm.lab
   }
 
-  depends_on = [module.networking_vault, module.sp_vault_access]
+  depends_on = [module.networking_vault]
 }
 
 # --------------------------------------------------
@@ -426,7 +364,6 @@ module "security_variable_group" {
     "securityspobjectid",
     "storageaccount",
     "tenantid",
-    "vaultname",
     "securityvaultname",
     "githubtoken"
   ]
