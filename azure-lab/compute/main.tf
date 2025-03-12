9f6f25b7ab61bf556472ed7f4fe2655f1c3d6cb1
Jon Hill <jonhill90@live.com>
2025-03-12 19:29:51 -0400

    removing noise from compute

--- azure-lab/compute/main.tf
+++ azure-lab/compute/main.tf
@@ -6,48 +6,12 @@ terraform {
 # Resource Groups (local)
 # ----------------------------------------
 resource "azurerm_resource_group" "lab" {
-  name     = "Compute-Lab"
+  name     = "Compute"
   location = "eastus"
   provider = azurerm.lab
 
   tags = {
     environment = var.environment
-    owner       = var.owner
-    project     = var.project
-  }
-}
-
-resource "azurerm_resource_group" "dev" {
-  name     = "Compute-Dev"
-  location = "eastus"
-  provider = azurerm.lab
-
-  tags = {
-    environment = "Dev"
-    owner       = var.owner
-    project     = var.project
-  }
-}
-
-resource "azurerm_resource_group" "test" {
-  name     = "Compute-Test"
-  location = "eastus"
-  provider = azurerm.lab
-
-  tags = {
-    environment = "Test"
-    owner       = var.owner
-    project     = var.project
-  }
-}
-
-resource "azurerm_resource_group" "prod" {
-  name     = "Compute-Prod"
-  location = "eastus"
-  provider = azurerm.lab
-
-  tags = {
-    environment = "Prod"
     owner       = var.owner
     project     = var.project
   }
