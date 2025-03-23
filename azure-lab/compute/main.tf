a44d4df55064abd58bf5b0813cfd66d1a645d85f
Jon Hill <jonhill90@live.com>
2025-03-22 20:59:51 -0400

    Rebuilding DC

--- azure-lab/compute/main.tf
+++ azure-lab/compute/main.tf
@@ -130,7 +130,7 @@ module "build_agent" {
   ]
 }
 */
-/*
+
 module "dc01" {
   source = "../../modules/azurerm/compute/vm/windows/dc"
 
@@ -157,4 +157,3 @@ module "dc01" {
     azurerm_shared_image.windows_2025_core
   ]
 }
-*/
