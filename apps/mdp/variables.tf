5b95d41ab3788a200abb5fc6220bd0119dd319bb
Jon Hill <jonhill90@live.com>
2025-04-05 19:46:20 -0400

    Starting MDP

---
+++ apps/mdp/variables.tf
@@ -0,0 +1,32 @@
+# ----------------------------------------
+#region Tags
+# ----------------------------------------
+variable "environment" {
+  description = "Environment Name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "owner" {
+  description = "Owning Team"
+  type        = string
+}
+
+variable "project" {
+  description = "Project Name"
+  type        = string
+}
+
+# ----------------------------------------
+#region Azure Settings
+# ----------------------------------------
+variable "lzp1_subscription_id" {
+  description = "Landing zone P1 Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+  sensitive   = true
+}
