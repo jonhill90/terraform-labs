9d8124edc0e4843c196bff75bb341014d1a214bf
Jon Hill <jonhill90@live.com>
2025-04-05 22:48:56 -0400

    Notebooks publishing data to Data Factory storage account

--- azure/datafactory/main.tf
+++ azure/datafactory/main.tf
@@ -54,3 +54,30 @@ data "azurerm_storage_container" "datafactory" {
   storage_account_name  = data.azurerm_storage_account.datafactory.name
   provider             = azurerm.lzp1
 }
+
+# ----------------------------------------
+#region Linked Services (ls)
+# ----------------------------------------
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "blob_ls" {
+  name                = "ls-blob-${var.environment}-lzp1"
+  data_factory_id     = azurerm_data_factory.df_lzp1.id
+  connection_string   = data.azurerm_storage_account.datafactory.primary_connection_string
+  provider            = azurerm.lzp1
+}
+
+# ----------------------------------------
+#region Datasets (ds)
+# ----------------------------------------
+resource "azurerm_data_factory_dataset_binary" "json_scraper_data" {
+  name                = "ds-json-scraper-${var.environment}-lzp1"
+  data_factory_id     = azurerm_data_factory.df_lzp1.id
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.blob_ls.name
+  folder              = "datasets"
+  provider            = azurerm.lzp1
+
+  azure_blob_storage_location {
+    container = data.azurerm_storage_container.datafactory.name
+    path      = "scraper/json/"
+    filename  = "tolkien_events.json"
+  }
+}
